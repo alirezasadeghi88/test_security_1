85662dd44ab897ffbbb6eb712459b9e9dd342a3a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-28 09:43:59 +0330

    Add

---
+++ src/main/java/com/learn/test_security_1/TestSecurity1Application.java
@@ -0,0 +1,13 @@
+package com.learn.test_security_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecurity1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSecurity1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/test_security_1/TestSecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.test_security_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecurity1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
