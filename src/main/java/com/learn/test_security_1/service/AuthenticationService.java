c333105b5e97f331ef7508db813961fee9530175
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-31 09:49:45 +0330

    Add class AuthenticationService

--- src/main/java/com/learn/test_security_1/service/AuthenticationService.java
+++ src/main/java/com/learn/test_security_1/service/AuthenticationService.java
@@ -1,4 +1,13 @@
 package com.learn.test_security_1.service;
 
+import com.learn.test_security_1.dto.*;
+
 public interface AuthenticationService {
+    LoginResponse register(RegisterRequest request);
+
+    LoginResponse login(LoginRequest request);
+
+    RefreshTokenResponse refreshToken(RefreshTokenRequest request);
+
+    void logout(LogoutRequest request);
 }
