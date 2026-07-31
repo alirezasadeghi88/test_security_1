8c343afb378ab2ca4f88c8d8354d8b71fb9b3f42
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-31 09:46:16 +0330

    Add class RegisterRequest

---
+++ src/main/java/com/learn/test_security_1/dto/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.learn.test_security_1.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+
+}
