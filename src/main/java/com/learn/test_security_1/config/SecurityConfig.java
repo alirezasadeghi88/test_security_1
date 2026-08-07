4092c1dc3c9636a8117af52dbd6e38dea03d4241
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-07 09:18:55 +0330

    Add class SecurityConfig

--- src/main/java/com/learn/test_security_1/config/SecurityConfig.java
+++ src/main/java/com/learn/test_security_1/config/SecurityConfig.java
@@ -1,4 +1,13 @@
 package com.learn.test_security_1.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 }
