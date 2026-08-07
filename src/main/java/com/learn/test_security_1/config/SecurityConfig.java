698c08a98e7e282d2d856e0c4a00b4fd0011e6f2
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-07 09:26:29 +0330

    Add class SecurityConfig

--- src/main/java/com/learn/test_security_1/config/SecurityConfig.java
+++ src/main/java/com/learn/test_security_1/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.learn.test_security_1.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,5 +28,12 @@ public class SecurityConfig {
                 new DaoAuthenticationProvider(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
